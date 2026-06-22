f193747b4df8272e38112aaf8d6739907dbb9200
Carol Ribeiro <anacarolina.ribeiro.s@gmail.com>
2026-06-22 20:45:24 -0300

    fix: shift header logo left to compensate image whitespace

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -86,7 +86,7 @@ export function Header({ onOpenCart }: HeaderProps) {
               alt="FEITORIA"
               width={160}
               height={50}
-              className="w-[120px] sm:w-[160px] h-auto object-contain"
+              className="w-[120px] sm:w-[160px] h-auto object-contain -translate-x-4 sm:-translate-x-6"
               priority
             />
           </Link>
